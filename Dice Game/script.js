f15eba85bd4ee6cf35b81b8d7983214b44f301b5
Ifty-Rahman <ifty09rahman@gmail.com>
2025-08-04 20:57:22 +0600

    updated js

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -2,17 +2,17 @@
 let firstRandomNumber = Math.floor(Math.random() * 6) + 1;
 let secondRandomNumber = Math.floor(Math.random() * 6) + 1;
 
-
+// Change the image source based on the random number
 document.querySelector("img.img1").setAttribute("src", "images/dice" + firstRandomNumber + ".png");
 document.querySelector("img.img2").setAttribute("src", "images/dice" + secondRandomNumber + ".png");
 
 //If player 1 wins
 if (firstRandomNumber > secondRandomNumber) {
-    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
 }
 //If player 2 wins
 else if (firstRandomNumber < secondRandomNumber) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
 }
 //If it's a draw
 else {
