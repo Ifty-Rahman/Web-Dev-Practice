3e5509d64bed7cf04712be2ecdecf91afcc5684b
Ifty-Rahman <ifty09rahman@gmail.com>
2025-08-10 22:37:18 +0600

    commit

--- Band Name Generator/index.js
+++ Band Name Generator/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   const randomAdj = adj[Math.floor(Math.random() * adj.length)];
   const randomNoun = noun[Math.floor(Math.random() * noun.length)];
-  res.render("solution.ejs", {
+  res.render("index.ejs", {
     adjective: randomAdj,
     noun: randomNoun,
   });
