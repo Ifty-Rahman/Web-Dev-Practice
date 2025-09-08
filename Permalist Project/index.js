9edf470c011a1da8d78a4dc555f1ce8d210f07f8
Ifty-Rahman <ifty09rahman@gmail.com>
2025-09-08 22:54:23 +0600

    permalist project

--- Permalist Project/index.js
+++ Permalist Project/index.js
@@ -1,34 +1,77 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "11919",
+  port: 5432,
+});
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+let items = [];
 
-app.get("/", (req, res) => {
+async function checkList() {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+  } catch (err) {
+    console.log(err);
+  }
+  return items;
+}
+
+app.get("/", async (req, res) => {
+  const items = await checkList();
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, "0");
+  const mm = String(today.getMonth() + 1).padStart(2, "0");
+  const yyyy = today.getFullYear();
   res.render("index.ejs", {
-    listTitle: "Today",
+    listTitle: "Date: " + dd + "-" + mm + "-" + yyyy,
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try {
+    await db.query("UPDATE items SET title = $1 WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
