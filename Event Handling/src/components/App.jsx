37523d6e451adc76d8de55128a64ba36e4f21e58
Ifty Rahman <Ifty09rahman@gmail.com>
2025-10-03 23:13:03 +0600

    even handling

---
+++ Event Handling/src/components/App.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+  const [isMouseOver, setMouseOver] = useState(false);
+
+  function handleClick(event) {
+    setHeadingText(name);
+
+    event.preventDefault();
+  }
+
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleChange(event) {
+    console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  return (
+    <div className="container">
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+        />
+        <button
+          type="submit"
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
